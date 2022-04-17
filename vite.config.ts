2c691acf03468c55011fa03b3336e7c73394babd
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2022-04-16 22:52:36 -0500

    📱 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: 'EmailGram',
+  plugins: [
+    svelte(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'https://apis.google.com/js/client.js'],
+      manifest: {
+        name: 'EmailGram',
+        short_name: 'EmailGram',
+        description: 'View emails in a social media/text message format',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      },
+    }),
+  ],
+  base: '/EmailGram/',
 })
