76b2f90651fada60b0bb7cab44987a51cfac6add
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2023-08-13 00:22:25 -0500

    Redirect to account

--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,1 +1,7 @@
-TODO: if authenticated, redirect. Else show about page & link to accounts page
+<meta http-equi ="refresh" content="2; url=#/account" />
+<script lang="ts">
+import {link} from 'svelte-spa-router';
+</script>
+
+<!-- TODO: if authenticated, redirect. Else show about page & link to accounts page -->
+<a href="/account" use:link>Go to app</a>
